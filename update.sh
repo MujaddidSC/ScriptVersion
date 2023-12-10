2dd08b4e0c1930691ff191298cd3cdc5848eafac
MujaddidSC <139568531+MujaddidSC@users.noreply.github.com>
2023-12-10 21:58:37 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,51 @@
+#!/bin/bash
+#Script Updater By MujaddidSC
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
 
+if [[ -e /usr/local/bin/xray ]]; then
+         echo ""
+         echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Already Install !"
+	 sleep 1
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Skip Update XRAY Core & Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm restore
+	 rm menu
+	 wget -O restore "https://raw.githubusercontent.com/MujaddidSC/MultiportWebsocketPremium/main/BACKUP/restore.sh" && chmod +x restore
+	 wget -O menu "https://raw.githubusercontent.com/MujaddidSC/MultiportWebsocketPremium/main/SSH/menu.sh" && chmod +x menu
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
+else
+         echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Update Starting !"
+	 sleep 1
+         mv /usr/local/bin/xray /usr/local/bin/xray.bak && wget -q -O /usr/local/bin/xray "https://github.com/dharak36/Xray-core/releases/download/v1.0.0/xray.linux.64bit" && chmod 755 /usr/local/bin/xray
+	 echo ""
+         echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Successfully Install !"
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Please ${red}Restart${NC} All VPN Services After Autoscript Patch Update Is Done . . ."
+	 sleep 3
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm restore
+	 rm menu
+	 wget -O restore "https://raw.githubusercontent.com/MujaddidSC/MultiportWebsocketPremium/main/BACKUP/restore.sh" && chmod +x restore
+	 wget -O menu "https://raw.githubusercontent.com/MujaddidSC/MultiportWebsocketPremium/main/SSH/menu.sh" && chmod +x menu
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
+
+fi
